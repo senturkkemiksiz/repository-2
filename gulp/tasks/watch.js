c08e04656acb1a218c6b47ef0b0a21b048fb8cb3
Senturk <senturkkemiksiz@gmail.com>
2019-02-03 12:04:35 +0300

    My last change

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+	
+	watch('./app/index.html', function () {
+		//gulp.start('html');
+		//html();
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', gulp.series('styles', 'cssInject'));
+
+});
+/*
+function cssInject() {
+	return gulp.src('./app/temp/styles/main.css')
+	.pipe(browserSync.stream());
+};
+*/
+gulp.task('cssInject', function() {
+  return gulp.src('./app/temp/styles/main.css')
+	.pipe(browserSync.stream());
+});
