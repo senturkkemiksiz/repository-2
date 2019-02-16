c83c1a2722d99d102e5c50fa3cc76f51fa990ef4
Senturk <senturkkemiksiz@gmail.com>
2019-02-16 18:45:39 +0300

    header part 1

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,8 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 /*
 function styles() {
 	return gulp.src('./app/assets/styles/main.css')
@@ -14,7 +15,7 @@ function styles() {
 */
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/main.css')
-	.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 	.on('error', function(errorInfo) {
 		console.log(errorInfo.toString());
 		this.emit('end');
