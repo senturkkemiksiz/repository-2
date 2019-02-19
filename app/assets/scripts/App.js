7cdd2de92dbc0dde85c1b519c05ed96542f24dc3
Senturk <senturkkemiksiz@gmail.com>
2019-02-19 23:10:07 +0300

    cool javascript code started

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes 0 dolars in taxes.");
-	}
-}
-
-//alert("hoppala hjşksjdfsd");
-
-var john = new Person("John Doe", "Blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "Orange");
-jane.greet();
-jane.payTaxes();
+var mobilMenu = new MobileMenu();
