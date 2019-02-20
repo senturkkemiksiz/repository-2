d1921215b444d02dc42c86415b277fdc5a145ff4
Senturk <senturkkemiksiz@gmail.com>
2019-02-20 13:37:43 +0300

    introduction to waypoints

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobilMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
